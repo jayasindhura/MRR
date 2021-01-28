00002183660dac2ac6f5b66dc84f02dcaa870301
jayasindhura <jpailla@unomaha.edu>
2021-01-28 17:58:50 -0600

    heroku deploy

--- MRR/settings.py
+++ MRR/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,4 +126,11 @@ AUTH_USER_MODEL = "mymrr.User"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL ='/media/'
+
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CLM.wsgi --log-file -
